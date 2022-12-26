ed1d6ea8dcd88f822153f6c3841e7a2c6cb031c5
Vardhanreddy-17 <110156702+Vardhanreddy-17@users.noreply.github.com>
2022-12-26 13:50:45 +0000

    no

---
+++ datatypes.py
@@ -0,0 +1,19 @@
+# print("HELLO"[4])
+# num_char = str(len(input("WHAT IS YOUR NAME ? ")))
+# # new_num_char = str(num_char)
+# print("YOUR NAME HAS "+num_char+" CHARACTERS")
+
+# Integer
+a=12234
+print(type(a))
+
+# float
+a=1234.55
+print(type(a))
+
+#Boolean
+True
+False
+
+print(str(20)+str(30))
+print(20+float("50.6"))
--- main.py
+++
@@ -1,4 +0,0 @@
-print("HELLO"[4])
-num_char = len(input("WHAT IS YOUR NAME ? "))
-new_num_char = str(num_char)
-print("YOUR NAME HAS "+new_num_char+" CHARACTERS")
